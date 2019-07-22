9352ff8c8b162d60e824b8538d34363c4745cf27
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-07-22 14:06:39 -0700

    some changes

--- client/src/Job.jsx
+++ client/src/Job.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import NewJob from './NewJob'
 
-class Profile extends React.Component {
+class Job extends React.Component {
    render() {
       return (
          <>
             <h1>Current Jobs:</h1>
+            <NewJob />
             <div><button>My Calendar</button></div>
             <div><button>My Docs</button></div>
             <div><button>Update Your Info</button></div>
--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NewJob from './NewJob'
+
 
 class Profile extends React.Component {
    render() {
@@ -8,17 +8,15 @@ class Profile extends React.Component {
          <>
             <h1>User Profile/Info:</h1>
 
-         </>
 
          <div>
-            <h1>Current Jobs:</h1>
-            {/* <NewJob /> */}
-            {/* <Link to="/newjob"><button className='btn'>Add A New Job Post</button></Link> */}
+           
             <button>My Calendar</button>
             <button>My Docs</button>
             <button>Update Your Info</button>
             
          </div>
+         </>
       )
    }
 }
