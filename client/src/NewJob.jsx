7e0afd6c16865c139af32841f42fab1e4a1dbc52
Anthony Riojas <anthonyr@Anthonys-MacBook-Pro-2.local>
2019-07-22 14:00:27 -0700

    small changes to fix my error codes

--- client/src/NewJob.jsx
+++ client/src/NewJob.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Profile extends React.Component {
+class NewJob extends React.Component {
    render() {
       return (
          <>
--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -7,18 +7,12 @@ class Profile extends React.Component {
 
          <>
             <h1>User Profile/Info:</h1>
-
-         </>
-
          <div>
-            <h1>Current Jobs:</h1>
-            {/* <NewJob /> */}
-            {/* <Link to="/newjob"><button className='btn'>Add A New Job Post</button></Link> */}
             <button>My Calendar</button>
             <button>My Docs</button>
             <button>Update Your Info</button>
-            
          </div>
+         </>
       )
    }
 }
