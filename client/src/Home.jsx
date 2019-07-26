ddcf0640e4e1b6b5dc4686e6684c9d8b06ca59ce
Anthony Riojas <anthonyr@Anthonys-MacBook-Pro-2.local>
2019-07-26 11:39:02 -0700

    working on css

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 const Home = props => (
    <>
       <div className="cover">
+      <body className="background">
       <h1>Welcome to the JobTrackker</h1>
       
       <p></p>
+      </body>
       </div>
    </>
 )
