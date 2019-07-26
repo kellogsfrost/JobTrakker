8ec8ffec03150f9b7382538612ae06f707ec4450
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-07-26 10:22:49 -0700

    working on last route

--- client/src/Interview.jsx
+++ client/src/Interview.jsx
@@ -8,19 +8,8 @@ class Interview extends React.Component {
         super(props);
         this.state = {
             interviews: [],
-            newDate: '',
-            newTime: '',
-            newInterviewer: '',
-            newNotes: '',
-            newLocation: '',
             token: ''
         }
-        this.newInterviewDate = this.newInterviewDate.bind(this);
-        this.newInterviewTime = this.newInterviewTime.bind(this);
-        this.newInterviewInterviewer = this.newInterviewInterviewer.bind(this);
-        this.newInterviewLocation = this.newInterviewLocation.bind(this);
-        this.newInterviewNotes = this.newInterviewNotes.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
       
     }
     componentDidMount() {
@@ -39,24 +28,7 @@ class Interview extends React.Component {
                 })
             })
     }
-    handleSubmit(e) {
-        e.preventDefault()
-        let userId = this.props.user._id;
-        axios.defaults.headers.common['Authorization'] = `Bearer ${this.props.token}` 
-        axios.post(`/api/profile/${userId}/interviews/`, {
-            date: this.state.newDate,
-            time: this.state.newTime,
-            interviewer: this.state.newInterviewer,
-            location: this.state.newLocation,
-            notes: this.state.newNotes
-        }).then((response) => {
-            axios.get(`/api/profile/${userId}/interviews`).then((response) => {
-                this.setState({
-                    interviews: response.data
-                })
-            })
-        })
-    }
+    
     handleMap(e) {
         e.preventDefault()
         axios.defaults.headers.common['Authorization'] = `Bearer ${this.props.token}` 
@@ -70,37 +42,6 @@ class Interview extends React.Component {
         })
     }
 
-
-
-    newInterviewDate(e) {
-        this.setState({
-            newDate: e.target.value
-        })
-    }
-
-    newInterviewTime(e) {
-        this.setState({
-            newTime: e.target.value
-        })
-    }
-
-    newInterviewInterviewer(e) {
-        this.setState({
-            newInterviewer: e.target.value
-        })
-    }
-    newInterviewLocation(e) {
-        this.setState({
-            newLocation: e.target.value
-        })
-    }
-
-    newInterviewNotes(e) {
-        this.setState({
-            newNotes: e.target.value
-        })
-    }
-
     render() {
         return (
 
@@ -108,25 +49,7 @@ class Interview extends React.Component {
                 <h1>Current Interviews:</h1>
                 <InterviewList interviews={this.state.interviews} />
                 <hr />
-                <h2>Create a New Interview!</h2>
-                <form onSubmit={this.handleSubmit}>
-                    Location:<br />
-                    <input value={this.state.newLocation} onChange={this.newInterviewLocation} type="text" />
-                    <br />
-                    Interviewer:<br />
-                    <input value={this.state.newInterviewer} onChange={this.newInterviewInterviewer} type="text" />
-                    <br />
-                    Date:<br />
-                    <input value={this.state.newDate} onChange={this.newInterviewDate} type="text" />
-                    <br />
-                    Time:<br />
-                    <input value={this.state.newTime} onChange={this.newInterviewTime} type="text" />
-                    <br />
-                    Notes:<br />
-                    <input value={this.state.newNotes} onChange={this.newInterviewNotes} type="text" />
-                    <br />
-                    <input type='submit' value="Save" />
-                </form>
+                
 
             </>
         )
--- client/src/JobDetail.jsx
+++ client/src/JobDetail.jsx
@@ -3,12 +3,28 @@ import axios from 'axios';
 import InterviewList from './InterviewList';
 
 class JobDetail extends React.Component {
-   state = {
-      jobInfo: {interviews:[]}
+   constructor(props) {
+      super(props);
+      this.state = {
+         interviews: [],
+         newDate: '',
+         newTime: '',
+         newInterviewer: '',
+         newNotes: '',
+         newLocation: '',
+         token: '',
+         jobInfo: { interviews: [] }
+      }
+      this.newInterviewDate = this.newInterviewDate.bind(this);
+      this.newInterviewTime = this.newInterviewTime.bind(this);
+      this.newInterviewInterviewer = this.newInterviewInterviewer.bind(this);
+      this.newInterviewLocation = this.newInterviewLocation.bind(this);
+      this.newInterviewNotes = this.newInterviewNotes.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
    }
    componentDidMount() {
       var jobId = this.props.match.params.id
-      
+
 
       console.log("hello" + this.props.match.params.id)
       axios.get(`/api/jobs/${jobId}`)
@@ -21,6 +37,54 @@ class JobDetail extends React.Component {
             console.log(this.state.jobInfo)
          })
    }
+   handleSubmit(e) {
+      e.preventDefault()
+      let jobId = this.props.match.params.id;
+      console.log(this.props.token)
+      axios.defaults.headers.common['Authorization'] = `Bearer ${this.props.token}`
+      axios.post(`/api/jobs/${jobId}/`, {
+         date: this.state.newDate,
+         time: this.state.newTime,
+         interviewer: this.state.newInterviewer,
+         location: this.state.newLocation,
+         notes: this.state.newNotes
+      }).then((response) => {
+         axios.get(`/api/jobs/${jobId}/`).then((response) => {
+            this.setState({
+               interviews: response.data
+            })
+         })
+      })
+   }
+
+   newInterviewDate(e) {
+      this.setState({
+         newDate: e.target.value
+      })
+   }
+
+   newInterviewTime(e) {
+      this.setState({
+         newTime: e.target.value
+      })
+   }
+
+   newInterviewInterviewer(e) {
+      this.setState({
+         newInterviewer: e.target.value
+      })
+   }
+   newInterviewLocation(e) {
+      this.setState({
+         newLocation: e.target.value
+      })
+   }
+
+   newInterviewNotes(e) {
+      this.setState({
+         newNotes: e.target.value
+      })
+   }
    render() {
       return (
          <>
@@ -31,7 +95,27 @@ class JobDetail extends React.Component {
             {this.state.jobInfo.phone}
             {this.state.jobInfo.email}
             <h1>Here are your Interviews for this job:</h1>
-            < InterviewList interviews={this.state.jobInfo.interviews}/>
+            < InterviewList interviews={this.state.jobInfo.interviews} />
+            <hr />
+            <h2>Create a New Interview!</h2>
+            <form onSubmit={this.handleSubmit}>
+               Location:<br />
+               <input value={this.state.newLocation} onChange={this.newInterviewLocation} type="text" />
+               <br />
+               Interviewer:<br />
+               <input value={this.state.newInterviewer} onChange={this.newInterviewInterviewer} type="text" />
+               <br />
+               Date:<br />
+               <input value={this.state.newDate} onChange={this.newInterviewDate} type="text" />
+               <br />
+               Time:<br />
+               <input value={this.state.newTime} onChange={this.newInterviewTime} type="text" />
+               <br />
+               Notes:<br />
+               <input value={this.state.newNotes} onChange={this.newInterviewNotes} type="text" />
+               <br />
+               <input type='submit' value="Save" />
+            </form>
          </>
       )
    }
